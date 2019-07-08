dc627133917bd0e607a7a2490189755ef2367a95
samiraabouda <samir.aabouda@live.fr>
2019-07-08 07:56:01 +0200

    Code fonctionnel apres MAJ Mysql de 5.7 vers 8

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import requests
+import json
 import mysql.connector
 from config import M_user,M_password
 
@@ -12,16 +14,21 @@ def execute_scripts_from_file(cursor,filename):
     sqlFile = fd.read()
     fd.close()
 
+   
 
     # Eliminer les commantaires et les fin de ligne
-    sqlFile = "".join(line for line in sqlFile.split("\n") if not line.startswith('--'))
+    sqlFile = "".join(line for line in sqlFile.split("\n")\
+    if not line.startswith('--'))
 
     # Eliminer les lignes vides
     sqlCommands = [line.strip() for line in sqlFile.split(";") if line.strip()]
 
     # Execute every command from the input file
     for command in sqlCommands:
-
+        # This will skip and report errors
+        # For example, if the tables do not yet exist, this will skip over
+        # the DROP TABLE commands
+        
         print(command)
         cursor.execute(command)
         print("Command executed")
@@ -32,9 +39,9 @@ def execute_scripts_from_file(cursor,filename):
 
 
 cnx = mysql.connector.connect\
-(user=M_user, password=M_password, host='localhost')
+(user=M_user, password=M_password, host='localhost',\
+auth_plugin='mysql_native_password')
 cursor = cnx.cursor()
-
 
 execute_scripts_from_file(cursor, "sql_script_purbeurre.sql")
 
