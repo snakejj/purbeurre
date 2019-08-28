222972139745afdbe2f6a7463aaefc0a89c42780
samiraabouda <samir.aabouda@live.fr>
2019-08-28 09:58:17 +0200

    Mise en place de l'interface terminal

--- index.py
+++ index.py
@@ -43,7 +43,7 @@ def filling_category_db(cursor):
     cursor.execute("""SELECT * FROM Category""")
 
     rows_count = cursor.fetchall()
-    print(rows_count)
+#    print(rows_count)
 
     if not rows_count:
         try:
@@ -64,44 +64,73 @@ def filling_category_db(cursor):
             
             print("Error while inserting categories in DB")
     else:
-        print("Categories already in the DB")
+        print("Categories already in the DB !")
 
     
-def sorting_product(cursor,category):
+def sorting_and_filling_product(cursor,category):
 
     
     
-    response = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?search_terms2={}&action=process&json=1&page_size=20".format(category))
+    response = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?search_terms2={}&action=process&json=1&page_size=100".format(category))
     result = json.loads(response.text)
 
+#    cursor.execute('SELECT food_id FROM Food WHERE category_id = 2')
+#    rows_count = cursor.fetchall()
+#    print(rows_count)
+
+#    if not rows_count:
+#        try:
+#            print("Row count est vide")
     cursor.execute('SELECT category_id FROM Category WHERE category_name = (%s)', (category,))
     numid = cursor.fetchone()
-    print(numid[0])
-    
-
+    #    print(numid[0])
     for p in result['products'] :
-        if p['code'] and p['product_name_fr'] and p['url'] and p['stores'] and p['ingredients_text_fr'] and p['nutrition_grades_tags'] and len(p['nutrition_grades_tags'][0]) == 1:
+        if p['code'] and 4 < len(p['product_name_fr']) < 80 and len(p['url']) < 255 and 1 < len(p['stores']) < 150 and p['ingredients_text_fr'] and len(p['nutrition_grades_tags'][0]) == 1:
             
-#            cursor.execute("INSERT INTO Food (nutriscore) VALUES (%s)", (numid[0], ))
-            print(numid[0])
-            print(p['nutrition_grades_tags'][0])
+#            print(p['nutrition_grades_tags'][0])
+#            print(p['stores'])
             cursor.execute("INSERT INTO Food (category_id, food_name, ingredients_text, store, off_link, nutriscore) VALUES (%s, %s, %s, %s, %s, %s)", (numid[0], p['product_name_fr'], p['ingredients_text_fr'], p['stores'], p['url'], p['nutrition_grades_tags'][0], ))
             
-            
-#
 
-#            print(p['code'],p['product_name_fr'],p['url'], p['stores'],p['ingredients_text_fr'],p['nutrition_grades_tags'][0]) 
                     
             cnx.commit()
-        
-        else:
-            print("Incomplet !") 
-        
-
-
+#        except:
+            
+#            print("EROOOOR") 
+     
+#    else :
+#            print("else")       
     
+        
+#        else:
+#            print("Incomplet !") 
+        
 
-#def filling_product_db():
+def categ_select():
+
+    cursor.execute('SELECT * FROM Category')
+    result = cursor.fetchall()
+    for a,b in result:
+        print(a,":",b)
+    menu_choice = int(input("Enter le numéro de la categorie souhaité : "))
+    
+    while menu_choice < 1 or menu_choice > 5 :
+        
+        menu_choice = int(input("Enter le numéro de la categorie souhaité : "))              
+
+    else :
+
+        product_select(menu_choice)
+        
+
+def product_select(numcateg):
+
+    cursor.execute('SELECT food_id, food_name FROM Food WHERE category_id = (%s)', (numcateg,))
+    result = cursor.fetchall()
+    for a,b in result:
+        print(a,":",b)
+    pass
+
 
     
 
@@ -125,17 +154,17 @@ try:
                                   db ='PurBeurre',
                                   auth_plugin='mysql_native_password')
         
-    print("\nConnected successfully !\n")
+    print("Connected successfully !\n")
     
     cursor = cnx.cursor()
-    print("\nOuverture du cursor !\n")
+#    print("Cursor opening !\n")
     
         
         
 except mysql.connector.Error as error:
 
-    print("\nFailed to connect to the database : Database does not exist.\n")
-    print("\nCreation of the database ...\n")
+    print("Failed to connect to the database : Database does not exist.\n")
+    print("Creation of the database ...\n")
     
     cnx = mysql.connector.connect(user=M_user, 
                                   password=M_password,
@@ -143,12 +172,12 @@ except mysql.connector.Error as error:
                                   auth_plugin='mysql_native_password')
 
     cursor = cnx.cursor()
-    print("\nOuverture du cursor lors du else !\n")
+#    print("Cursor opening in else !\n")
     
     try:
         
         execute_sql_from_file(cursor, "sql_script_purbeurre.sql")
-        print("\nDatabase created with success !\n")
+        print("Database created with success !\n")
         
     except:
         
@@ -164,9 +193,52 @@ except mysql.connector.Error as error:
 
 if connected :
     filling_category_db(cursor)
-    sorting_product(cursor, "Boissons")
+    sorting_and_filling_product(cursor, "Yaourts")
+    sorting_and_filling_product(cursor, "Chocolats")
+    sorting_and_filling_product(cursor, "Boissons")
+    sorting_and_filling_product(cursor, "Snacks")
+    sorting_and_filling_product(cursor, "Produits laitiers")
 
 
+####################################
+# TERMINAL INTERFACE
+####################################  
+
+print("""
+
+##############################################################
+
+      """)
+
+ongoing = True
+
+
+
+print("1 - Quel aliment souhaitez-vous remplacer ?")
+print("2 - Retrouver mes aliments substitués.")
+print("3 - Quitter !")
+    
+menu_choice = int(input("Enter le numéro de l'option souhaité : "))
+
+while menu_choice < 1 or menu_choice > 3 :
+    menu_choice = int(input("Enter le numéro de l'option souhaité : "))
+    
+if menu_choice == 1 :
+        
+    categ_select()
+              
+
+elif menu_choice == 2 :
+
+    print("En cours de dev")
+        
+
+elif menu_choice == 3 :
+    print("Au revoir")
+    
+
+    
+    
 
 
 
